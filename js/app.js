631d6403f46cf23d36fc926417f86a51baf9f249
isha0207 <isha95419@gmail.com>
2026-03-18 22:03:46 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -63,7 +63,7 @@ if (canvas) {
       labels: ["A", "B", "C" , "D" , "E" , "F" , "G" , "H" , "I" , "J"],
       datasets: [{
         label: "Test",
-        data: [10, 20, 30]
+        data: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
       }]
     }
   });
